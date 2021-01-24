e97627b5779e4a74dcb256ffa9afeab1d5c1957d
Wryz <57204327+Wryz@users.noreply.github.com>
2021-01-24 15:05:30 -0500

    Basic Dungeon Grid layout

--- src/main/java/net/siegemc/core/Dungeons/DungeonGrid.java
+++ src/main/java/net/siegemc/core/Dungeons/DungeonGrid.java
@@ -1,5 +1,27 @@
 package net.siegemc.core.Dungeons;
 
+import org.bukkit.Location;
+import java.util.ArrayList;
+
 public class DungeonGrid {
+
+    static ArrayList<Location> dungeonGrid = new ArrayList<Location>(); //Dungeon grid of locations
+
+
+    public static void dungeonAddLoc(){ //find next available grid point and add schematic and coordinate to the array list
+
+    }
+    public static void dungeonDelLoc(){ //deletes the grid point at the index of the parameter of the array list
+
+    }
+    public static void dungeonViewLoc(){ //returns the location of the grid point in the array
+
+    }
+    public static void dungeonDelGrid(){ //deletes all dungeons and stored grid points
+
+    }
+    public static void dungeonViewGrid(){ //returns all the grid points of active dungeons
+
+    }
 
 }
