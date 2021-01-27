b75ba23f309788e2fdaeb1dc4842f6e8ed53b3b9
Bagellls <77936535+Bagellls@users.noreply.github.com>
2021-01-27 14:10:54 +0000

    Update DungeonGrid.java

--- src/main/java/net/siegemc/core/Dungeons/DungeonGrid.java
+++ src/main/java/net/siegemc/core/Dungeons/DungeonGrid.java
@@ -7,31 +7,44 @@ import org.bukkit.Location;
 import java.util.HashMap;
 
 public class DungeonGrid {
-    private static HashMap<Location, Dungeon> dungeonGrid = new HashMap<>(); // Dungeon grid of locations
+    private static final HashMap<Location, Dungeon> dungeonGrid = new HashMap<>(); // Dungeon grid of locations
+    public static int maxGridPoints = 25; // Example size
 
     // Call when updating the tasks or other for the dungeon Object.
     public void setDungeonEntry(Location location, Dungeon dungeon) { // Sets an existing entry dungeon object to something new. Used for updating tasks etc.
-        if (!dungeonGrid.keySet().contains(location)) return;
+        if (!dungeonGrid.containsKey(location)) return;
         dungeonGrid.replace(location, dungeon); // Not 100% sure if this overwrites properly. PLEASE CHECK
         // Alternative dungeonGrid.replace(location, olddungeon, newdungeon);
     }
 
     // Call when needing to create a dungeon.
-    public void dungeonCreate(Dungeon dungeon){ //add schematic and coordinate of the next available grid point to the array list
+    public Location dungeonCreate(Dungeon dungeon){ //add schematic and coordinate of the next available grid point to the array list
         int Index = dungeonGrid.isEmpty() ? 1 : dungeonGrid.size();
-        int locationX = (Index / 4) * 500;
-        int locationY = 120;
-        int locationZ = (Index % 4) * 500;
-        Location location = new Location(Bukkit.getServer().getWorld("Dungeons"), locationX, locationY, locationZ); // "Dungeons" is a placeholder for the world name.
+
+        int locationX = (Index / 5) * 500;
+        int locationZ = (Index % 5) * 500;
+
+        Location location = new Location(Bukkit.getServer().getWorld("Dungeons"), locationX, 120, locationZ); // "Dungeons" is a placeholder for the world name.
+        if (!dungeonGrid.isEmpty()) {
+            int X;
+            for (int i = 0; i <= dungeonGrid.size(); i++) {
+                if (dungeonGrid.containsKey(location)) {
+                    X = i / 5;
+                    location.set(X * 500, 120, (i % 5) * 500);
+                }
+                else { break; }
+            }
+        } // Could do with a more efficient method than this for loop... Replace if possible
         dungeonGrid.put(location, dungeon);
-        //SchematicPaster.pasteSchematic("", location, "",true); // FILL THE PARAMS HERE!!! <<< Don't forget to uncomment this.
+        return location;
+        //SchematicPaster.pasteSchematic("", location, "", true); // FILL THE PARAMS HERE!!! <<< Don't forget to uncomment this.
         //This information for the schematic needs to be filled.
     }
 
     // Call once a dungeon has been completed.
-    public void dungeonDelete(Location location){ //deletes the grid point via the location object#
+    public void dungeonDelete(Location location){ //deletes the grid point via the Location object
         if (dungeonGrid.isEmpty()) return;
-        if (!dungeonGrid.keySet().contains(location)) return;
+        if (!dungeonGrid.containsKey(location)) return;
         dungeonGrid.remove(location);
     }
     // Call when you feel like being a meanie and deleting everyone's dungeon data.
@@ -40,11 +53,6 @@ public class DungeonGrid {
     }
 
     // Call if you need the dungeon location (For NPC location etc) Returns an Object not a Location.
-    public Object getDungeonEntry(int Index){ //returns the location via the Index (Not recommended, if even possible at all)
-        if (dungeonGrid.isEmpty()) { return null; }
-        if (dungeonGrid.size() < Index) { return null; }
-        return dungeonGrid.keySet().toArray()[Index]; // Unsure of this, may not work. PLEASE CHECK
-    }
     public Location getDungeonEntry(Dungeon dungeon){ //returns the location via the dungeon object
         if (dungeonGrid.isEmpty()) { return null; }
         for (Entry<Location, Dungeon> singleEntry : dungeonGrid.entrySet()) {
@@ -53,7 +61,7 @@ public class DungeonGrid {
         return null;
     }
 
-    // Call for the amount of active dungeons, just for fun. All methods depend on the grid being not empty.
+    // Call for the amount of active dungeons, not required. All methods depend on the grid being not empty.
     public int getDungeonGrid(){ //returns the amount of active dungeons from the grid (All dungeons from 1 to Index are active currently).
         if (dungeonGrid.isEmpty()) return 0;
         return dungeonGrid.keySet().size();
