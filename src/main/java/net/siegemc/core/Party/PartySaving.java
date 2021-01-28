52e12d023d68eee8594704fbf6d4573b1a724afc
Itayg <galilitay@gmail.com>
2021-01-28 22:59:56 +0200

    Test yaml
    testing yaml

---
+++ src/main/java/net/siegemc/core/Party/PartySaving.java
@@ -0,0 +1,32 @@
+package net.siegemc.core.Party;
+
+import net.siegemc.core.Core;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PartySaving {
+    public static FileConfiguration yaml = null;
+    public static File PartyData;
+    public static void FileExists() {
+        PartyData = new File(Core.dataFolder, "PartyData.yml");
+        YamlConfiguration yaml = YamlConfiguration.loadConfiguration(PartyData);
+        if (!PartyData.exists()) {
+            try {
+                PartyData.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        yaml.createSection("Test");
+        yaml.set("test", "hello");
+        try {
+            yaml.save(PartyData);;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
