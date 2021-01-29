a8bcd94b956173bfc61c2fe4c1b407e5d7a4632c
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-01-29 17:13:00 +0100

    Testing something

--- src/main/java/net/siegemc/core/Testing.java
+++ src/main/java/net/siegemc/core/Testing.java
@@ -1,12 +1,11 @@
 package net.siegemc.core;
 
-import net.siegemc.core.dungeons.DungeonBosses;
 import org.junit.Test;
 
 public class Testing {
     @Test
     public static void main(String[] args) {
-        System.out.println(DungeonBosses.dungeonBoss(1));
+        System.out.println("Test");
     }
 }
 
