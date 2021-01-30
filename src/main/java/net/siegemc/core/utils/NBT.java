936d02b95ad675357d167bbbea645579d8b920bd
SantioMC <SantioTheBest123@gmail.com>
2021-01-30 16:00:46 -0500

    Add NBT util

---
+++ src/main/java/net/siegemc/core/utils/NBT.java
@@ -0,0 +1,36 @@
+package net.siegemc.core.utils;
+
+import net.siegemc.core.Core;
+import org.bukkit.NamespacedKey;
+import org.bukkit.persistence.PersistentDataHolder;
+import org.bukkit.persistence.PersistentDataType;
+
+public class NBT {
+    public static NamespacedKey generateKey(String key) {
+        return new NamespacedKey(Core.plugin(), key);
+    }
+    
+    public static void addInt(PersistentDataHolder holder, String key, int value) {
+        holder.getPersistentDataContainer().set(generateKey(key), PersistentDataType.INTEGER, value);
+    }
+    
+    public static void addDouble(PersistentDataHolder holder, String key, double value) {
+        holder.getPersistentDataContainer().set(generateKey(key), PersistentDataType.DOUBLE, value);
+    }
+    
+    public static void addString(PersistentDataHolder holder, String key, String value) {
+        holder.getPersistentDataContainer().set(generateKey(key), PersistentDataType.STRING, value);
+    }
+    
+    public static int getInt(PersistentDataHolder holder, String key) {
+        return holder.getPersistentDataContainer().getOrDefault(generateKey(key), PersistentDataType.INTEGER, 0);
+    }
+    
+    public static double getDouble(PersistentDataHolder holder, String key) {
+        return holder.getPersistentDataContainer().getOrDefault(generateKey(key), PersistentDataType.DOUBLE, 0D);
+    }
+    
+    public static String getString(PersistentDataHolder holder, String key) {
+        return holder.getPersistentDataContainer().getOrDefault(generateKey(key), PersistentDataType.STRING, null);
+    }
+}
